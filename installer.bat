cf8d23fa4c50d6f0316b2afc186c6aba804e4d3d
GreenDevil1 <136200524+GreenDevil1@users.noreply.github.com>
2023-06-23 21:30:28 +0200

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -1,4 +1,4 @@
 @echo off
-SCHTASKS /CREATE /SC ONLOGON /TN "start_music_after_login" /TR "C:\Users\%USERNAME%\Startup\music_on_startup.bat" /DELAY 0000:10 /RL HIGHEST
-SCHTASKS /CREATE /SC ONLOGON /TN "start_WallpaperEngine_after_login" /TR "C:\Users\%USERNAME%\Startup\Wallpaper_Engine_on_startup.bat" /DELAY 0000:30 /RL HIGHEST
-SCHTASKS /CREATE /SC ONLOGON /TN "start_blockiplist_after_login" /TR "C:\Users\%USERNAME%\Startup\Datei3.bat" /DELAY 0003:00 /RL HIGHEST
+SCHTASKS /CREATE /SC ONLOGON /TN "start_music_after_login" /TR "C:\Users\%USERNAME%\Startup\Startup-Windows\music_on_startup.bat" /DELAY 0000:10 /RL HIGHEST
+SCHTASKS /CREATE /SC ONLOGON /TN "start_WallpaperEngine_after_login" /TR "C:\Users\%USERNAME%\Startup\Startup-Windows\Wallpaper_Engine_on_startup.bat" /DELAY 0000:30 /RL HIGHEST
+SCHTASKS /CREATE /SC ONLOGON /TN "start_blockiplist_after_login" /TR "C:\Users\%USERNAME%\Startup\Startup-Windows\firewall.bat" /DELAY 0003:00 /RL HIGHEST
