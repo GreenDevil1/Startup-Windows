086ae2ad74ea699e3edbda88f79f3201863bcef7
GreenDevil1 <136200524+GreenDevil1@users.noreply.github.com>
2023-06-23 21:34:21 +0200

    new

--- Wallpaper_Engine_on_startup.bat
+++ Wallpaper_Engine_on_startup.bat
@@ -4,14 +4,14 @@ start /d "C:/Program Files (x86)/Steam/steamapps/common/wallpaper_engine/" wallp
 timeout /t 5
 if %hour% GEQ 8 if %hour% LEQ 14 (
     timeout /t 5
-    start "" "C:/Users/green/Documents/Python/Startup/Key_Press/8to14.vbs"
+    start "" "C:/Users/%USERNAME%/startup/Startup-Windows/Key_Press/8to14.vbs"
 ) else if %hour% GEQ 15 if %hour% LEQ 21 (
     timeout /t 5
-    start "" "C:/Users/green/Documents/Python/Startup/Key_Press/15to21.vbs"
+    start "" "C:/Users/%USERNAME%/startup/Startup-Windows/Key_Press/15to21.vbs"
 ) else if %hour% GEQ 22 if %hour% LEQ 24 (
     timeout /t 5
-    start "" "C:/Users/green/Documents/Python/Startup/Key_Press/22to7.vbs"
+    start "" "C:/Users/%USERNAME%/startup/Startup-Windows/Key_Press/22to7.vbs"
 ) else if %hour% GEQ 1 if %hour% LEQ 7 (
     timeout /t 5
-    start "" "C:/Users/green/Documents/Python/Startup/Key_Press/22to7.vbs"
+    start "" "C:/Users/%USERNAME%/startup/Startup-Windows/Key_Press/22to7.vbs"
 )
--- music_on_startup.bat
+++ music_on_startup.bat
@@ -5,11 +5,11 @@ set /a rand2=%random% %% 3 + 1
 set /a rand3=%random% %% 3 + 1
 set /a rand4=%random% %% 3 + 1
 if %hour% GEQ 8 if %hour% LEQ 14 (
-    start /d "C:/Program Files (x86)/Windows Media Player/" wmplayer.exe "C:/Users/green/Documents/Python/Startup/Music_8_to_14/%rand1%.wav"
+    start /d "C:/Program Files (x86)/Windows Media Player/" wmplayer.exe "C:/Users/%USERNAME%/startup/Startup-Windows/Music_8_to_14/%rand1%.wav"
 ) else if %hour% GEQ 15 if %hour% LEQ 21 (
-    start /d "C:/Program Files (x86)/Windows Media Player/" wmplayer.exe "C:/Users/green/Documents/Python/Startup/Music_15_to_21/%rand2%.wav"
+    start /d "C:/Program Files (x86)/Windows Media Player/" wmplayer.exe "C:/Users/%USERNAME%/startup/Startup-Windows/Music_15_to_21/%rand2%.wav"
 ) else if %hour% GEQ 22 if %hour% LEQ 24 (
-    start /d "C:/Program Files (x86)/Windows Media Player/" wmplayer.exe "C:/Users/green/Documents/Python/Startup/Music_22_to_7/%rand3%.wav"
+    start /d "C:/Program Files (x86)/Windows Media Player/" wmplayer.exe "C:/Users/%USERNAME%/startup/Startup-Windows/Music_22_to_7/%rand3%.wav"
 ) else if %hour% GEQ 1 if %hour% LEQ 7 (
-    start /d "C:/Program Files (x86)/Windows Media Player/" wmplayer.exe "C:/Users/green/Documents/Python/Startup/Music_22_to_7/%rand4%.wav"
+    start /d "C:/Program Files (x86)/Windows Media Player/" wmplayer.exe "C:/Users/%USERNAME%/startup/Startup-Windows/Music_22_to_7/%rand4%.wav"
 )
